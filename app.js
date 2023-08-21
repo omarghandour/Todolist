e75d8e59151ac6a201ff924e7c92c75a986e1519
omarghandour <109479370+omarghandour@users.noreply.github.com>
2023-08-21 17:29:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ passport.deserializeUser(function (user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/todo"
+    callbackURL: "https://ghandour.fun/auth/todo"
 },
     function (accessToken, refreshToken, profile, cb) {
         const gname = profile.displayName
